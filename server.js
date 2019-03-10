b386f98d360c26a9d074c0ca74484725db07b60f
Glitch (space-unit) <none>
2019-03-10 02:21:13 +0000

    server force https

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 require('./socket.js')(io);
 app.use(express.static('public'));
+app.use(function(req, res, next) {
+      if ((req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+      } else
+        next();
+    });
 app.get("/", function (request, response) {
   response.sendFile('./public/index.html',{root:'.'});
 }); 
